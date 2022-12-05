88c76477941b9d5796f3abe1404248346fdb5d86
johnyalam <johnyalam.lut@gmail.com>
2022-12-06 00:21:19 +0200

    React app Frontend created and components folder created with Header and Footer js.

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <footer>Header</footer>
+    </div>
+  );
+}
+
+export default Header;
